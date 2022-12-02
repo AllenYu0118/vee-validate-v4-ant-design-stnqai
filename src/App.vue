16ada3aa00318b8d8976bf65f5349d0b521156b4
AllenYu0118 <yulei0118@gmail.com>
2022-12-02 19:09:08 +0800

    2 files modified

--- src/App.vue
+++ src/App.vue
@@ -30,7 +30,7 @@
     <!-- Do this if you plan to have validation frequently throughout your app -->
     <AntInputWithValidation name="password" label="Password" type="password" />
 
-    <!-- <Area name="area" v-model="area" /> -->
+    <Area name="area" />
 
     <a-form-item>
       <a-button type="primary" html-type="submit">Submit</a-button>
@@ -43,17 +43,18 @@ import { defineComponent, ref } from 'vue';
 import { Field, Form } from 'vee-validate';
 import * as yup from 'yup';
 import AntInputWithValidation from './components/AntInputWithValidation.vue';
-// import Area from './components/Area.vue';
+import Area from './components/Area.vue';
 
-// const area = ref({
-//   area: null,
-//   unit: 1,
-// });
+const area = ref({
+  area: null,
+  unit: 1,
+});
 
 const schema = yup.object({
   email: yup.string().required().email().label('Email address'),
   fullName: yup.string().required().label('Full name'),
   password: yup.string().required().min(6).label('Password'),
+  area: yup.string().required().min(6).label('area'),
 });
 
 const onSubmit = (values) => {
--- src/components/Area.vue
+++ src/components/Area.vue
@@ -3,48 +3,32 @@
     label="車位坪數"
     required
     :auto-link="false"
-    :help="errorMsg"
-    :validate-status="errorMsg ? 'error' : ''"
+    :help="errorMessage"
+    :validate-status="errorMessage ? 'error' : ''"
   >
     <a-input-group compact class="w-280px! inline-block!">
       <a-input-number
-        v-model:value="houseArea.area"
+        v-model:value="value"
         class="w-180px!"
         :controls="false"
         maxlength="6"
-        @blur="emits('validate', 'area')"
       />
-      <a-select
-        v-model:value="houseArea.unit"
-        class="w-100px!"
-        :options="options"
-      >
-      </a-select>
     </a-input-group>
   </a-form-item>
 </template>
 
 <script lang="ts" setup>
-import { useVModel } from '@vueuse/core';
+import { toRef } from 'vue';
+import { useField } from 'vee-validate';
 
-const emits = defineEmits(['validate']);
-const props = defineProps<{
-  modelValue: {
-    area: number;
-    unit: number;
-  };
-}>();
-
-const houseArea = useVModel(props, 'modelValue');
-
-const options = [
-  {
-    label: '坪',
-    value: 1,
+const props = defineProps({
+  name: {
+    type: String,
+    required: true,
   },
-  {
-    label: '平方公尺',
-    value: 2,
-  },
-];
+});
+
+const { errorMessage, value } = useField(toRef(props, 'name'));
+
+console.log('value', value);
 </script>
